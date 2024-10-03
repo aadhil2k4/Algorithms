e3f48b58965bcc02281e80c0304ca4a82d2f36fa
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-10-03 23:23:18 +0530

    knapsack-->memoize

--- DynamicProgramming/0/0-1_Knapsack.cpp
+++ DynamicProgramming/0/0-1_Knapsack.cpp
@@ -8,7 +8,7 @@ int recursive(vector<int> &wt, vector<int>&val, int w, int n){
     if(n==0 || w==0){
         return 0;
     }
-    //Select item
+    //Item can be selected
     if(wt[n-1]<=w){
         return max(val[n-1]+ recursive(wt, val, w-wt[n-1], n-1), recursive(wt, val, w, n-1));
     }
@@ -18,7 +18,21 @@ int recursive(vector<int> &wt, vector<int>&val, int w, int n){
     }
 }
 
-
+int memoize(vector<int>&wt, vector<int>&val, int w, int n, vector<vector<int>>&dp){
+    if(n==0 || w==0){
+        return 0;
+    }
+    if(dp[n][w]!=-1){
+        return dp[n][w];
+    }
+    if(wt[n-1]<=w){
+        dp[n][w] = max(val[n-1]+memoize(wt,val,w-wt[n-1],n-1,dp), memoize(wt,val,w,n-1,dp));
+    }
+    else if(wt[n-1]>w){
+        dp[n][w] = memoize(wt,val,w,n-1,dp);
+    }
+    return dp[n][w];
+}
 
 int main(){
 
