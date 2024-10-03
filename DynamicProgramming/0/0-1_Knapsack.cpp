5fed578f3d39c27e530017ff9803190cca3c03dd
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-10-04 00:59:49 +0530

    knapsack--->tabulation

--- DynamicProgramming/0/0-1_Knapsack.cpp
+++ DynamicProgramming/0/0-1_Knapsack.cpp
@@ -34,6 +34,38 @@ int memoize(vector<int>&wt, vector<int>&val, int w, int n, vector<vector<int>>&d
     return dp[n][w];
 }
 
-int main(){
+int tabulate(vector<int>&wt, vector<int>&val, int w,int n){
+    vector<vector<int>>t(n+1, vector<int>(w+1));
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=w; j++){
+            if(i==0 || j==0){
+                t[i][j]=0;
+            }
+            if(wt[i-1]<=j){
+                t[i][j] = max(val[i-1]+t[i-1][j-wt[i-1]], t[i-1][j]);
+            }
+            else if(wt[i-1]>j){
+                t[i][j] = t[i-1][j];
+            }
+        }
+    }
+    return t[n][w];
+}
 
+int main(){
+    int n, w;
+    cout << "Enter the number of items: ";
+    cin >> n;
+    vector<int> wt(n), val(n);
+    cout << "Enter the weight and value of each item:\n";
+    for(int i = 0; i < n; i++){
+        cin >> wt[i] >> val[i];
+    }
+    cout << "Enter the maximum capacity of the knapsack: ";
+    cin >> w;
+    vector<vector<int>> dp(n+1, vector<int>(w+1, -1));
+    cout << "Maximum value using recursion: " << recursive(wt, val, w, n) << endl;
+    cout << "Maximum value using memoization: " << memoize(wt, val, w, n, dp) << endl;
+    cout << "Maximum value using tabulation: " << tabulate(wt, val, w, n) << endl;
+    return 0;
 }
