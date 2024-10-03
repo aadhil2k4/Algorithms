777636f48be0496b85c9ee3f3dbef982298b7488
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-10-03 23:09:26 +0530

    knapsack->recursive

--- DynamicProgramming/0/0-1_Knapsack.cpp
+++ DynamicProgramming/0/0-1_Knapsack.cpp
@@ -3,6 +3,23 @@
 
 using namespace std;
 
+int recursive(vector<int> &wt, vector<int>&val, int w, int n){
+    //Base Condition
+    if(n==0 || w==0){
+        return 0;
+    }
+    //Select item
+    if(wt[n-1]<=w){
+        return max(val[n-1]+ recursive(wt, val, w-wt[n-1], n-1), recursive(wt, val, w, n-1));
+    }
+    //Reject item
+    else if(wt[n-1]>w){
+        return recursive(wt, val, w, n-1);
+    }
+}
+
+
+
 int main(){
-    
+
 }
