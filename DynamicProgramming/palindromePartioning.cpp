6f8704b13fd181da5db30a2b88ac34c164eaab85
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-10-11 01:10:28 +0530

    minimum deletion to palindrome

---
+++ DynamicProgramming/palindromePartioning.cpp
@@ -0,0 +1,16 @@
+//MCM based
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int partition(string x, int n){
+
+}
+
+int main(){
+    string x = "nitin";
+    int n =x.length();
+    int ans = partition(x,n);
+    cout << ans;
+}
